e101f6380c20d863d1dfc3f5bd01e0c03840bb18
harrisjt <hjthompson@live.com>
2018-09-29 10:16:32 -0500

    add: rest api

---
+++ src/main/java/com/predict/data/controller/RestApiController.java
@@ -0,0 +1,27 @@
+package com.predict.data.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class RestApiController {
+
+  @Autowired
+  public RestApiController() {
+    //this.userRepository = userRepository;
+  }
+
+  //@CrossOrigin(origins = "http://localhost:8080")
+  @RequestMapping(value = "/register", method = RequestMethod.POST)
+  public ResponseEntity<Question> register(@RequestParam(value = "username") String username, @RequestParam(value = "password") String password) {
+
+   // return new ResponseEntity<>(this.userRepository.save(user), HttpStatus.OK);
+  }
+
+}
