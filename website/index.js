549ca84d32ca0b44e57f47fd28c1e64eb418361e
marshmallowchess <javarduinogames@gmail.com>
2018-09-29 16:08:17 -0500

    Removed unnecessary package reference

--- website/index.js
+++ website/index.js
@@ -1,10 +1,8 @@
-const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res) {
   res.render('index')
