7995b10f33e7ebb0b0d1dd7ef3fd9bdb4b6fa87c
marshmallowchess <javarduinogames@gmail.com>
2018-09-29 16:47:39 -0500

    Added email tests

---
+++ src/test/java/com/predict/data/EmailTests.java
@@ -0,0 +1,26 @@
+package com.predict.data;
+
+import com.predict.data.util.EmailManager;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmailTests {
+
+    private static final String SUBJECT = "Testing";
+    private static final String CONTENT = "Automated testing message";
+
+    @Test
+    public void testSendEmail() {
+        String[] recipients = new String[] { "predictapp.noreply@gmail.com" };
+        assert EmailManager.sendEmail(recipients, SUBJECT, CONTENT);
+    }
+
+    @Test
+    public void testSendMassEmail() {
+        assert EmailManager.sendEmailToAllUsers(SUBJECT, CONTENT);
+    }
+}
