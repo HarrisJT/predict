bb3a6be4ede8a66494c36379a23d92e620da6870
elliotBraem <braemelliot@gmail.com>
2018-09-29 14:57:29 -0500

    feat: CreateQuestion extension of SurveyMonkeyService

--- src/main/java/com/predict/data/service/SurveyService.java
+++ src/main/java/com/predict/data/service/SurveyService.java
@@ -1,98 +1,79 @@
 package com.predict.data.service;
 
+import br.com.devfast.jsurveymonkey.app.SurveyConfig;
+import br.com.devfast.jsurveymonkey.enums.StatusSurveyResponse;
 import br.com.devfast.jsurveymonkey.request.CreateSurveyRequest;
-import br.com.devfast.jsurveymonkey.request.GetSurveyRequest;
 import br.com.devfast.jsurveymonkey.response.CreateSurveyResponse;
-import br.com.devfast.jsurveymonkey.response.GetSurveyResponse;
 import br.com.devfast.jsurveymonkey.services.SurveyMonkeyService;
+import com.predict.data.entity.builder.CreateQuestionResponseBuilder;
+import com.predict.data.entity.request.CreateQuestionRequest;
+import com.predict.data.entity.response.CreateQuestionResponse;
 import com.predict.data.util.ConfigManager;
 import com.predict.data.entity.Question;
-import com.predict.data.util.ParameterStringBuilder;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
+import java.net.*;
 
-public class SurveyService {
+@Service
+public class SurveyService extends SurveyMonkeyService {
 
     private static final String API_AUTH_TOKEN;
-    private static final String SM_API_BASE;
 
+    private SurveyMonkeyService surveyService;
 
     @Autowired
-    SurveyMonkeyService surveyMonkeyService;
-
     public SurveyService() {
-        URL url = null;
-        HttpURLConnection con = null;
-        try {
-            url = new URL(SM_API_BASE);
-            con = (HttpURLConnection) url.openConnection();
-            con.setRequestMethod("GET");
-            con.setRequestProperty("Content-Type", "application/json");
-            con.setRequestProperty("Authorization", "Bearer " + API_AUTH_TOKEN + "s");
-
-            Map<String, String> parameters = new HashMap<>();
-            parameters.put("param1", "val");
-            parameters.put("param1", "val");
-            parameters.put("param1", "val");
-            parameters.put("param1", "val");
-
-            con.setDoOutput(true);
-            DataOutputStream out = new DataOutputStream(con.getOutputStream());
-            out.writeBytes(ParameterStringBuilder.getParamsString(parameters));
-            out.flush();
-            out.close();
-
-
-
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (ProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-
-
-
-//        SurveyMonkeyService surveyMonkeyService = new SurveyMonkeyService(API_AUTH_TOKEN);
-        this.surveyMonkeyService = surveyMonkeyService;
+        SurveyMonkeyService surveyService = new SurveyMonkeyService(API_AUTH_TOKEN);
+        this.surveyService = surveyService;
     }
 
     static {
         API_AUTH_TOKEN = ConfigManager.getProperty("api_auth_token");
-        SM_API_BASE = ConfigManager.getProperty("sm_api_base");
     }
 
-//    public void newQuestion(Question question) {
-//
-//    }
+    public CreateQuestionResponse createQuestion(CreateQuestionRequest request) {
+        try {
 
-    public CreateSurveyResponse createSurvey(Question question) {
+            CloseableHttpClient httpClient = HttpClients.createDefault();
+            HttpPost httpPost = new HttpPost(new URI(SurveyConfig.ENDPOINT_V3 + SURVEY_SERVICE + ));
+
+            setRequestAuthentication(httpPost, request.getAuthenticationToken());
+            setRequestBody(httpPost, request.getJsonBody());
+
+            CloseableHttpResponse response = httpClient.execute(httpPost);
+            String result = EntityUtils.toString(response.getEntity());
+
+            setResponse(result);
+            return new CreateQuestionResponseBuilder(result).getResponse();
+
+        } catch (Exception e) {
+            return new CreateQuestionResponse(StatusSurveyResponse.ERROR, e.getMessage());
+        }
+    }
+
+
+    public void createSurvey(Question question) {
         CreateSurveyRequest createSurveyRequest = new CreateSurveyRequest();
         createSurveyRequest.setTitle(question.getCategory());
         createSurveyRequest.setNickname("New question from Predict");
 
-        CreateSurveyResponse createSurveyResponse = surveyMonkeyService.createSurvey(createSurveyRequest);
+        CreateSurveyResponse createSurveyResponse = surveyService.createSurvey(createSurveyRequest);
 
         System.out.println(createSurveyResponse.getId());
     }
 
-    public void sendSurvey(
+
 //
-    public void fetchSurvey() {
-        GetSurveyRequest getSurveyRequest = new GetSurveyRequest("ID_SURVEY");
-        GetSurveyResponse getSurveyResponse = surveyMonkeyService.getSurvey(getSurveyRequest);
-    }
+//    public void fetchSurvey() {
+//        GetSurveyRequest getSurveyRequest = new GetSurveyRequest("ID_SURVEY");
+//        GetSurveyResponse getSurveyResponse = surveyMonkeyService.getSurvey(getSurveyRequest);
+//    }
 
 
 
