da8f8ae9d3edeef2b20e0e5c807a6fbdfcc4a2f6
elliotBraem <braemelliot@gmail.com>
2018-09-30 01:42:03 -0500

    feat: User class created

--- src/main/java/com/predict/data/entity/User.java
+++ src/main/java/com/predict/data/entity/User.java
@@ -1,11 +1,23 @@
 package com.predict.data.entity;
 
+import java.util.Map;
+
 public class User {
 
   private String email;
+  private Map<String, Double> weights;
 
   public User(String email) {
     this.email = email;
+    this.weights.put("Inter_Politics", 0.1);
+    this.weights.put("Nation_Politics", 0.1);
+    this.weights.put("Economics", 0.1);
+    this.weights.put("Entertainment", 0.1);
+    this.weights.put("Sports", 0.1);
+    this.weights.put("Technology", 0.1);
+    this.weights.put("Biology", 0.1);
+    this.weights.put("Environment", 0.1);
+    this.weights.put("Natural_Disasters", 0.1);
   }
 
   public String getEmail() {
@@ -14,5 +26,13 @@ public class User {
 
   public void setEmail(String email) {
     this.email = email;
+  }
+
+  public Map<String, Double> getWeights() {
+    return weights;
+  }
+
+  public void setWeights(Map<String, Double> weights) {
+    this.weights = weights;
   }
 }
